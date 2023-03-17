7cd4119cfe237635ec8098fb41241f6245f6d1a8
OMagwala <omagwala19@gmail.com>
2023-03-17 06:49:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-int main() {
-	puts("\"Programming is like building a multilingual puzzle);
+#include <stdio.h>
+int main(){
+	puts("Programming is like building a multilingual puzzle \n");
 	return(0);
 }
