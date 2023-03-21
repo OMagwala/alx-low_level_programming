51a4dbe0350d22cc79d8aab2b08d330b69826669
OMagwala <omagwala19@gmail.com>
2023-03-21 14:43:13 -0700

    Task5

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even valued terms
+ * less than 4,000,000
+ * Return: nothing
+ */
+
+int main(void)
+{
+
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while ((j + k) < 4,000,000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
